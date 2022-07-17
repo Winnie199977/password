414b5c2ff98a505abcbe73b6067cbc9cf3482888
Winnie Lin <winnie771737@gmail.com>
2022-07-17 14:50:39 +0800

    change while condition

--- password.py
+++ password.py
@@ -3,7 +3,7 @@
 password = 'a1234'
 count = 3
 
-while True:
+while count > 0:
 	guess = input('請輸入密碼 : ')
 	if guess == password:
 		print('登入成功!')
@@ -11,7 +11,4 @@ while True:
 	else:
 		count = count - 1
 		print('密碼錯誤! 還有', count, '次機會')
-		if count == 0:
-			print('失敗!')
-			break
 		
