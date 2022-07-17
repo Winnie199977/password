79c9e0c13b315e6d6f065013c7f4561b735648cc
Winnie Lin <winnie771737@gmail.com>
2022-07-17 14:54:08 +0800

    update code

--- password.py
+++ password.py
@@ -4,11 +4,15 @@ password = 'a1234'
 count = 3
 
 while count > 0:
+	count = count - 1
 	guess = input('請輸入密碼 : ')
 	if guess == password:
 		print('登入成功!')
 		break
 	else:
-		count = count - 1
-		print('密碼錯誤! 還有', count, '次機會')
+		print('密碼錯誤!')
+		if count > 0:
+			print('還有', count, '次機會')
+		else:
+			print('失敗')
 		
